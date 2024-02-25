fa1af263872ef05a31852eb8f0f3ba3293547d2f
ANEES00 <aneesmohdkp@gmail.com>
2024-02-25 09:58:03 +0530

    Data transformation pipeline

--- src/textSummarizer/pipeline/stage_01_data_ingestion.py
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -9,12 +9,9 @@ class DataIngestionTrainingPipeline:
         pass
     
     def main(self):
-        try:
-            config = ConfigurationManager()
-            data_ingestion_config = config.get_data_ingestion_config()
-            data_ingestion = DataIngestion(config=data_ingestion_config)
-            data_ingestion.download_file()
-            data_ingestion.extract_zip_file()
-        except Exception as e:
-            raise e
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
     
--- src/textSummarizer/pipeline/stage_02_data_validation.py
+++ src/textSummarizer/pipeline/stage_02_data_validation.py
@@ -9,11 +9,8 @@ class ValidationTrainingPipeline:
         pass
     
     def main(self):
-        try:
-            config = ConfigurationManager()
-            data_validation_config = config.get_data_validation_config()
-            data_validation = DataValiadtion(config=data_validation_config)
-            data_validation.validate_all_files_exist()
-        except Exception as e:
-            raise e
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_files_exist()
     
